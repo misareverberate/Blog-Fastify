5d28012b423dc9aff2f381ab9e30974d89c036b5
Misa Reverberate <misareverberate@gmail.com>
2025-03-24 13:57:53 -0400

    Ajustes rotas

--- index.ts
+++ index.ts
@@ -8,8 +8,16 @@ connectToDatabase()
 fastify.register(multipart, {attachFieldsToBody: 'keyValues'})
 /**
  * Contém todas as informações de um post do blog
+ * 
  */
 interface Post{
+    /**
+     * O id do post, atualment, corresponde ao id da rota dentro do blog. 
+     * 
+     *  O id específico dentro do banco é o padrão _id gerado automaticamente
+     * 
+     * Exemplo: seublog.com/post/um-dia-no-Brasil
+     */
     id:string
     title:string
     description:string
@@ -20,9 +28,12 @@ interface Post{
     post_tags:string | string[]
 }
 
+//Criação de um post
 fastify.post('/create-post', async (request:FastifyRequest, reply:FastifyReply) => {
     const postData:Post = request.body as Post
+    //Pretendo avaliar outro formato de representar o id
     postData.id = postData.title.replaceAll(' - ', '-').replaceAll(' ', '-')
+    
     if (typeof postData.post_tags === 'string') {
         postData.post_tags = postData.post_tags.split(',')
     }
@@ -30,18 +41,17 @@ fastify.post('/create-post', async (request:FastifyRequest, reply:FastifyReply) 
         const newPost = new Post({...postData})
         await newPost.save()
     } catch (error) {
-        return reply.code(501).send({responde: `Erro ao salvar o post: ${error}`})
+        return reply.code(501).send({response: `Erro ao salvar o post: ${error}`})
     }
-    reply.code(201).send({response: 'Post Criado com Sucesso', data: postData})
+    reply.code(201).send({response: 'Post Criado com Sucesso'})
 }) 
 
 fastify.delete('/delete-post/', async (request:FastifyRequest, reply:FastifyReply) => {
     const {postId} = request.query as any
-    console.log(postId)
     try {
         await Post.deleteOne({id: postId})
     } catch (error) {
-        return reply.code(501).send({responde: `Erro ao apagar o post: ${error}`})
+        return reply.code(501).send({response: `Erro ao apagar o post: ${error}`})
     }
     reply.code(201).send({response: 'Post apagado com sucesso'})
 }) 
@@ -50,10 +60,10 @@ fastify.get('/post/:postId', async (request:FastifyRequest, reply:FastifyReply) 
     const {postId} = request.params as any
     try {
         const postToRetrieve:Post|null = await Post.findOne({id: postId})
-        if(!postToRetrieve) throw new Error('Post perdido no meio do caminho (-_-)')
-        reply.code(200).send({post: postToRetrieve})
+        if(!postToRetrieve) throw new Error('O Post solicitado não existe ou não está mais disponível (-_-)')
+        reply.code(200).send({response: postToRetrieve})
     } catch (error) {
-        return reply.code(400).send({responde: `Erro ao recuperar o post: ${error}`})
+        return reply.code(400).send({response: error.message})
     }
 })
 
@@ -65,11 +75,11 @@ fastify.put('/update-post/', async (request:FastifyRequest, reply:FastifyReply) 
         postData.post_tags = postData.post_tags.split(',')
     }
     try {
-        if(!(await Post.findOne({id: postId}))) return reply.code(400).send({response: 'O Post não existe (~_~)'})
+        if(!(await Post.findOne({id: postId}))) return reply.code(400).send({response: 'O Post socilitado não existe ou está indisponível (~_~)'})
         await Post.updateOne({id: postId}, postData)
         reply.code(201).send({response: 'Post atualizado com sucesso'})
     } catch (error) {
-        return reply.code(400).send({responde: `Erro ao atualizar o post: ${error}`})
+        return reply.code(400).send({response: `Erro ao atualizar o post: ${error.message}`})
     }
 })
 
@@ -81,10 +91,10 @@ fastify.get('/posts/', async (request:FastifyRequest, reply:FastifyReply) => {
         }
         const filter = tags.length ? { post_tags: { $in: tags } } : {};
         const postsToRetrieve: Post[] = await Post.find(filter);
-        if (!postsToRetrieve) throw new Error('Post perdido no meio do caminho (-_-)');
-        return reply.code(200).send({ response: 'Posts recuperados com sucesso', post: postsToRetrieve });
+        if (!postsToRetrieve) throw new Error('O Post solicitado não existe ou não está mais disponível (-_-)');
+        return reply.code(200).send({response: postsToRetrieve });
     } catch (error) {
-        return reply.code(400).send({ response: `Erro ao recuperar o post: ${error}` });
+        return reply.code(400).send({ response: error});
     }
 })
 
